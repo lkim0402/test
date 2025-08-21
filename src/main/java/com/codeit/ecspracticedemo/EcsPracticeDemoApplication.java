755866c21f15133b611394f3c6f8d89ed1a40cfd
Leejun <estherleejunkim@gmail.com>
2025-08-21 21:20:47 +0900

    create workflow

---
+++ src/main/java/com/codeit/ecspracticedemo/EcsPracticeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.ecspracticedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsPracticeDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EcsPracticeDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/codeit/ecspracticedemo/EcsPracticeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.ecspracticedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsPracticeDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
